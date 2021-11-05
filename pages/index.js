6a6034a49a356c6b79ac7fdcd4b68d0b5d788f1a
swanand2406 <swanand.sambhus@bluepineapple.io>
2021-11-05 18:23:13 +0530

    basic file

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,37 @@
 
-export const getStaticProps = async () => {
-  const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=e5aa86bd0e914921add0f3dadb0a99ad');
-  const data = await res.json();
-  const datao=data.articles[0];
-  //const myjson=JSON.parse(data)
-  console.log(data);
-  //console.log("Helllllllllllllllllllllllllllllloooooooooooooooooooooooooooooo",datao.title);
+// export const getStaticProps = async () => {
+//   const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=e5aa86bd0e914921add0f3dadb0a99ad');
+//   const data = await res.json();
+//   const datao=data.articles[0];
 
+//   return {
+//     props: { data }
+//   }
+// }
+
+function App({ data }) {
+  return (
+    <ul>
+      {data.map((key) => (
+        <li>{key.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=e5aa86bd0e914921add0f3dadb0a99ad')
+  const data = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
   return {
-    props: { datao: datao }
+    props: {
+      data:data.articles,
+    },
   }
 }
 
-const App = ({ datao }) => {
-  console.log(datao)
-
-  return (
-    <div>
-      
-      {datao.map(dat => (
-        <div key={dat.author}>
-          <a >
-            <h3>{ dat.title }</h3>
-          </a>
-        </div>
-      ))}
-    </div>
-  );
-}
- 
 export default App;
